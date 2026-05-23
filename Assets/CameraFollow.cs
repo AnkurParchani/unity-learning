c2343bf873dc32eb7ae6d6e97b8c9676ba6476c6
AnkurParchani <ankurparchani@gmail.com>
2026-05-23 22:12:58 +0530

    add camera follow for following the cube

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    void Update () {
+        transform.position = player.position + new Vector3(0, 2, -6);
+    }
+
+}
